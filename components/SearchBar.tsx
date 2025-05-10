a5a8f5e56316c4dc63547063ff7d64a642e4167e
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2025-05-10 20:00:19 +0200

    feat(home): display movies

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,10 +2,26 @@ import { View, Text, Image, TextInput } from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
 
-const SearchBar = () => {
+
+interface Props {
+    placeholder: string;
+    onPress: () => void;
+}
+
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={'#ab8bff'} />
+      <TextInput 
+        onPress={onPress}
+        placeholder={placeholder}
+        value=''
+        placeholderTextColor='#A8B5DB'
+        onChangeText={() => {}}
+        className='flex-1  text-white ml-2 '
+        
+       />
     </View>
   )
 }
