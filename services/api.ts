b74b49a46b7a40f3227797aeaa97af54f9ffe830
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2025-05-26 14:27:24 +0200

    feat: implement full movie details screen

--- services/api.ts
+++ services/api.ts
@@ -8,7 +8,6 @@ export const TMDB_CONFIG = {
 }
    
 
-
 export const fetchMovies = async ({query} : {query: string}) => {
     const endpoint = query 
         ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
@@ -30,4 +29,22 @@ export const fetchMovies = async ({query} : {query: string}) => {
 
     return data.results;
 
+}
+
+export const fetchMovieDetails = async (movieId: string): Promise<MovieDetails> => {
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
+            method: 'GET',
+            headers: TMDB_CONFIG.headers,
+        }); 
+
+        if (!response.ok) throw new Error('Failed to fetch movie details');
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
